86ca879e6ea3bc81878e42fd60324625bd4f8921
wenhao <minwenhao1992@gmail.com>
2021-04-01 10:02:29 -0700

    update load balancer

--- main.tf
+++ main.tf
@@ -477,6 +477,7 @@ resource "aws_security_group" "load_balancer" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_launch_configuration" "asg_launch_config" {
   name                        = "asg_launch_config"
   image_id                    = data.aws_ami.ubuntu.id
@@ -503,15 +504,17 @@ resource "aws_lb" "app_lb" {
   name               = "app-load-balancer"
   internal           = false
   load_balancer_type = "application"
+  ip_address_type    = "ipv4"
   security_groups    = [aws_security_group.load_balancer.id]
   subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id, aws_subnet.subnet3.id]
 }
 
 resource "aws_lb_target_group" "lb_target_group" {
-  name     = "lb-target-group"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc1234.id
+  name        = "lb-target-group"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc1234.id
+  target_type = "instance"
   health_check {
     path     = "/v1/users"
     port     = 8080
@@ -520,9 +523,8 @@ resource "aws_lb_target_group" "lb_target_group" {
 }
 resource "aws_lb_listener" "app_lb_listener_https" {
   load_balancer_arn = aws_lb.app_lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = "arn:aws:acm:us-east-1:231232113671:certificate/71144f2f-2c38-4ed7-8deb-c63c5a5f71ec"
+  port              = "80"
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
@@ -586,36 +588,28 @@ resource "aws_cloudwatch_metric_alarm" "CPUAlarmLow" {
   period             = "300"
   evaluation_periods = "2"
   threshold          = 70
-  alarm_actions      = [aws_autoscaling_policy.autoscaling_scale_up_policy.arn]
+  alarm_actions      = [aws_autoscaling_policy.autoscaling_scale_down_policy.arn]
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.aws_autoscale_gr.name
   }
   comparison_operator = "LessThanThreshold"
 }
-resource "aws_route53_record" "subdomin-ns" {
-  zone_id = var.profile == "prod" ? "Z0809633154F51HJX4K3" : "Z08074672NPNTYRSOVXG0"
-  name    = var.profile == "prod" ? "prod.wenhaom.me" : "dev.wenhaom.me"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.app_lb.dns_name
-    zone_id                = aws_lb.app_lb.zone_id
-    evaluate_target_health = false
-  }
-}
-
 resource "aws_codedeploy_app" "example" {
   compute_platform = "Server"
   name             = "csye6225-webapp"
 }
 resource "aws_codedeploy_deployment_group" "example" {
   app_name               = "csye6225-webapp"
-  deployment_config_name = "CodeDeployDefault.AllAtOnce"
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
   deployment_group_name  = "csye6225-webapp-deployment"
   service_role_arn       = aws_iam_role.CodeDeployServiceRole.arn
-
+  load_balancer_info {
+    target_group_info {
+      name = aws_lb_target_group.lb_target_group.name
+    }
+  }
   deployment_style {
-    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
     deployment_type   = "IN_PLACE"
   }
   ec2_tag_set {
@@ -630,5 +624,21 @@ resource "aws_codedeploy_deployment_group" "example" {
     events  = ["DEPLOYMENT_FAILURE"]
   }
 }
+
+
+
+resource "aws_route53_record" "subdomin-ns" {
+  zone_id = var.profile == "prod" ? "Z0809633154F51HJX4K3" : "Z08074672NPNTYRSOVXG0"
+  name    = var.profile == "prod" ? "prod.wenhaom.me" : "dev.wenhaom.me"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app_lb.dns_name
+    zone_id                = aws_lb.app_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
 
 
